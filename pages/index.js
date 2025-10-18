9ce68d6c757e9f0936efc053ef49cae92e61a503
TokaXD <pedro.almeidasousah@hotmail.com>
2025-10-18 17:18:52 -0300

    Enhance homepage layout with centered text and added GIF

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 function Home() {
-    return <h1>Hellen EU TE AMO DEMAIS, sem vc não sei o que fazer com minha vida e sei que vc é a melhor coisa que já aconteceu comigo ❤️</h1>
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h1>
+        Hellen EU TE AMO DEMAIS, sem vc não sei o que fazer com minha vida e sei
+        que vc é a melhor coisa que já aconteceu comigo ❤️
+      </h1>
+      <img src="/meu-amor.gif" alt="GIF de amor" />
+    </div>
+  );
 }
 
 export default Home;
