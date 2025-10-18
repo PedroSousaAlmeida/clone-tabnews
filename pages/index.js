f9606408cf6a039049f25d6e6f1d851bb6785f6d
TokaXD <pedro.almeidasousah@hotmail.com>
2025-10-18 16:57:28 -0300

    Update homepage message to a personal note

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Home Teste</h1>
+    return <h1>Hellen EU TE AMO DEMAIS, sem vc não sei o que fazer com minha vida e sei que vc é a melhor coisa que já aconteceu comigo ❤️</h1>
 }
 
 export default Home;
